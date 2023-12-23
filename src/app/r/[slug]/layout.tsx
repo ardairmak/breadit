d3ec0b54f63658ec978938c563d26a7ad0ed1101
Arda Irmak <ardairmak2003@gmail.com>
2023-12-24 00:46:11 +0300

    added back button

--- src/app/r/[slug]/layout.tsx
+++ src/app/r/[slug]/layout.tsx
@@ -1,32 +1,36 @@
 import SubscribeLeaveToggle from "@/components/SubscribeLeaveToggle";
+import ToFeedButton from "@/components/ToFeedButton";
 import { buttonVariants } from "@/components/ui/Button";
-import { INFINITE_SCROLLING_PAGINATION_RESULTS } from "@/config";
 import { getAuthSession } from "@/lib/auth";
 import { db } from "@/lib/db";
 import { format } from "date-fns";
+import type { Metadata } from "next";
 import Link from "next/link";
 import { notFound } from "next/navigation";
+import { ReactNode } from "react";
 
-const layout = async ({
+export const metadata: Metadata = {
+  title: "Breadit",
+  description: "A Reddit clone built with Next.js and TypeScript.",
+};
+
+const Layout = async ({
   children,
   params: { slug },
 }: {
-  children: React.ReactNode;
+  children: ReactNode;
   params: { slug: string };
 }) => {
   const session = await getAuthSession();
 
   const subreddit = await db.subreddit.findFirst({
-    where: {
-      name: slug,
-    },
+    where: { name: slug },
     include: {
       posts: {
         include: {
           author: true,
           votes: true,
         },
-        take: INFINITE_SCROLLING_PAGINATION_RESULTS,
       },
     },
   });
@@ -59,15 +63,16 @@ const layout = async ({
   return (
     <div className="sm:container max-w-7xl mx-auto h-full pt-12">
       <div>
-        {/*todo: button to take us back */}
+        <ToFeedButton />
+
         <div className="grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6">
-          <div className="flex flex-col col-span-2 space-y-6">{children}</div>
-          {/*info sidebar */}
-          <div className="hidden md:block overflow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last">
+          <ul className="flex flex-col col-span-2 space-y-6">{children}</ul>
+
+          {/* info sidebar */}
+          <div className="overflow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last">
             <div className="px-6 py-4">
               <p className="font-semibold py-3">About r/{subreddit.name}</p>
             </div>
-
             <dl className="divide-y divide-gray-100 px-6 py-4 text-sm leading-6 bg-white">
               <div className="flex justify-between gap-x-4 py-3">
                 <dt className="text-gray-500">Created</dt>
@@ -77,34 +82,31 @@ const layout = async ({
                   </time>
                 </dd>
               </div>
-
               <div className="flex justify-between gap-x-4 py-3">
                 <dt className="text-gray-500">Members</dt>
-                <dd className="text-gray-700">
+                <dd className="flex items-start gap-x-2">
                   <div className="text-gray-900">{memberCount}</div>
                 </dd>
               </div>
-
-              {subreddit.creatorId === session?.user.id ? (
+              {subreddit.creatorId === session?.user?.id ? (
                 <div className="flex justify-between gap-x-4 py-3">
-                  <p className="text-gray-500">You created this community</p>
+                  <dt className="text-gray-500">You created this community</dt>
                 </div>
               ) : null}
 
-              {subreddit.creatorId !== session?.user.id ? (
+              {subreddit.creatorId !== session?.user?.id ? (
                 <SubscribeLeaveToggle
+                  isSubscribed={isSubscribed}
                   subredditId={subreddit.id}
                   subredditName={subreddit.name}
-                  isSubscribed={isSubscribed}
                 />
               ) : null}
-
               <Link
-                href={`r/${slug}/submit`}
                 className={buttonVariants({
                   variant: "outline",
                   className: "w-full mb-6",
                 })}
+                href={`r/${slug}/submit`}
               >
                 Create Post
               </Link>
@@ -116,4 +118,4 @@ const layout = async ({
   );
 };
 
-export default layout;
+export default Layout;
